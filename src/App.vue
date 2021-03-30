9a860f72ee97b248f3d38a3808b1205977a5db51
Emile <emile.doyen@etu.u-bordeaux.fr>
2021-03-30 18:01:15 +0200

    Ajout d'un menu d'ajout de widget (WIP)

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,18 @@
   <div id="app">
     <Menu></Menu>
     <Meteo></Meteo>
+    <md-speed-dial :class="bottomPosition" id="add-widget">
+      <md-speed-dial-target>
+        <md-icon>add</md-icon>
+      </md-speed-dial-target>
+
+      <md-speed-dial-content>
+        <md-button class="md-icon-button">
+          <md-icon>nights_stay</md-icon>
+        </md-button>
+
+      </md-speed-dial-content>
+    </md-speed-dial>
   </div>
 </template>
 
@@ -27,5 +39,11 @@ export default {
   color: #2c3e50;
   min-height: 100vh;
   padding: 1rem;
+  position: relative;
+  #add-widget {
+    position: absolute;
+    right: 1rem;
+    bottom: 1rem;
+  }
 }
 </style>
