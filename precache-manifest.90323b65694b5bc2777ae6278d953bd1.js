f3c717ad0544cbfc1082dcfe64b7c77345a1aacd
Doyen Emile <emile.doyen@etu.u-bordeaux.fr>
2021-05-03 21:54:22 +0200

    deploy

---
+++ precache-manifest.90323b65694b5bc2777ae6278d953bd1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15bd3b6c883ee29865ab",
+    "url": "/iGoogle/css/app.f190f3a5.css"
+  },
+  {
+    "revision": "49417cc257ad7f9f236d",
+    "url": "/iGoogle/css/chunk-vendors.09b7d566.css"
+  },
+  {
+    "revision": "b6241d5140acd785fb446744d2452fb9",
+    "url": "/iGoogle/img/emile-doyen(NB).b6241d51.jpg"
+  },
+  {
+    "revision": "b0f057f3480026aaaa510546e2378ce9",
+    "url": "/iGoogle/img/grid.b0f057f3.svg"
+  },
+  {
+    "revision": "fe3acd262ee34b887e6070da1dc3bc1d",
+    "url": "/iGoogle/index.html"
+  },
+  {
+    "revision": "15bd3b6c883ee29865ab",
+    "url": "/iGoogle/js/app.fdcfd017.js"
+  },
+  {
+    "revision": "49417cc257ad7f9f236d",
+    "url": "/iGoogle/js/chunk-vendors.843ff9dc.js"
+  },
+  {
+    "revision": "6a69c9b21b66e212a74505434108b3ab",
+    "url": "/iGoogle/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/iGoogle/robots.txt"
+  }
+]);
